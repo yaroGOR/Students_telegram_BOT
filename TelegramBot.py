13860d01340bdd581d154dbfad0d0f41b6f18dd5
Ярослав Горбань <yarikgor98@gmail.com>
2021-03-06 23:20:25 +0200

    короткое сообщение, описывающее изменения в коммите

---
+++ TelegramBot.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+
+import telebot;
+import database;
+import sqlite3;
+import time
+import datetime
+import onlyPrivatModule as pb
+
+#-----Создание БД--------
+conn = sqlite3.connect('students.db',check_same_thread=False)
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS stud(
+   studid INTEGER PRIMARY KEY AUTOINCREMENT,
+   name TEXT,
+   amountLessons INT,
+   duration INT,
+   pay INT,
+   weekpayT INT,
+   monthpayT INT,
+   weekpayF INT,
+   monthpayF INT,
+   timetable TEXT)
+""")
+conn.commit()
+date=datetime.datetime.now()
+
+
+#------Работа бота-------
+bot = telebot.TeleBot('1575787181:AAHglwiHFUMRx593KarTi7vIQXdfroq8pTU');
+dic=[]
+#создание нового ученика в базу данных по команте /new, /start и добавление имени, количества уроков, длительности, оплаты в час
+#
+@bot.message_handler(commands=['create','new'])
+def newStudentName(message):
+	print(dic)
+	msg=bot.send_message(message.chat.id, "Введи имя ученика: ")
+	bot.register_next_step_handler(msg, setStudentName)
+
+def setStudentName(message):
+	stname = message.text;
+	bot.send_message(message.chat.id, "Имя ученика: "+stname);
+	msg=bot.send_message(message.chat.id, "Введите количество уроков: ");
+	bot.register_next_step_handler(msg, setAmountLessons);
+	print("name "+stname)
+	dic.append(stname)
+	print(type(stname))
+
+def setAmountLessons(message):
+	amount=message.text;
+	msg=bot.send_message(message.chat.id, "Количество уроков: "+amount);
+	askDuration = bot.send_message(message.chat.id, "Введи длительность урока: ")
+	bot.register_next_step_handler(msg, setDuration);
+	print("amount " + amount)
+	dic.append(amount)
+	print(type(amount))
+
+def setDuration(message):
+	duration=message.text;
+	if duration == "1":
+		text=" час"
+	elif duration == "1,5" or "1.5" or "2":
+		text = " часа"
+	msg=bot.send_message(message.chat.id, "Длительность уроков: "+duration+text);
+	bot.send_message(message.chat.id, "Введи стоимость за час: ")
+	print("duration " + duration)
+	bot.register_next_step_handler(msg, setPay);
+	dic.append(duration)
+#Добавление в базу данных
+def setPay(message):
+	pay=message.text;
+	msg=bot.send_message(message.chat.id, "Стоимость в час "+pay);
+	print("pay "+pay);
+	dic.append(pay)
+	print(dic)
+	payForWeek=float(dic[1])*float(dic[2])*float(dic[3])
+	print(payForWeek)
+	print(type(payForWeek))
+	dic.append(payForWeek)
+	payForMonth=payForWeek*4
+	dic.append(payForMonth)
+	bot.send_message(message.chat.id, "В неделю стоимость: "+str(payForWeek))
+	bot.send_message(message.chat.id, "В месяц стоимость: "+str(payForWeek*4))
+	msg=bot.send_message(message.chat.id, "Добавьте дни недели для занятий в формате:пн, пт, вт, ср, чт...несколько дней отделяются запятыми")
+	bot.register_next_step_handler(msg,setTimetable)
+#добавления словаря dic в БД
+def addStudtodb():
+	cur.execute("""INSERT INTO stud (name, amountLessons, duration, pay, weekpayT, monthpayT, timetable) VALUES(?,?,?,?,?,?,?)""", dic);
+	conn.commit();
+	print(dic)
+#очистка словаря с данными после добавления в БД
+	print(dic)
+	print(type(dic))
+	for i in dic[:]:
+	     dic.remove(i)
+
+#добавить дни занятий для ученика
+def setTimetable(message):
+	daysstr=message.text
+	dayslist=daysstr.split(",")
+	print(dayslist)
+	dic.append(daysstr)
+	print(dic)
+	addStudtodb()
+
+#выбор ученика по имени и выдача всех данных о нем
+@bot.message_handler(commands=['select'])
+def selectstud(message):
+	msg=bot.send_message(message.chat.id, "Введите имя ученика, чтобы получить данные об уроках")
+	bot.register_next_step_handler(msg,selectDB)
+
+def selectDB(message):
+	studName=message.text
+	cur.execute("SELECT * FROM stud WHERE name = '"+studName+"'")
+	data=cur.fetchall()
+	print(data)
+	if data==[]:
+		bot.send_message(message.chat.id, "Нету такого ученика. Добавьте или введите имя корректно")
+	else: pass
+	for row in data:
+
+		print(row)
+		print(type(row))
+		msgText=f"""Имя ученика: {row[1]}
+		Количество занятий:{row[2]}
+		Длительность занятий:{row[3]}
+		Ставка в час:{row[4]}
+		Стоимость занятия:{int(row[3]*int(row[4]))}
+		Стоимость в неделю:{row[5]}
+		Стоимость в месяц:{row[6]}
+		Дни занятий:{row[9]}"""
+		print(msgText)
+		bot.send_message(message.chat.id, msgText)
+
+
+
+
+#выписка из банка
+@bot.message_handler(commands=['trans'])
+#запрос даты с которой скачивать транзакции
+###добавить запрос даты которой заканчивать транзакции
+#Добавить сортировку по датам
+#исправить порядок сообщений по датам
+def askfordate(message):
+	msg=bot.send_message(message.chat.id, "Введите дату с которой скачать транзакции в формате дд.мм.гггг:" )
+	bot.register_next_step_handler(msg,setdate)
+def getTransactions(message):
+	msg=bot.send_message(message.chat.id, "Введите дату с которой скачать транзакции в формате дд.мм.гггг:" )
+	bot.register_next_step_handler(msg,setdate)
+def setdate(message):
+	datestr=message.text
+		#datestr.split(',')
+	print(datestr)
+	date=datetime.datetime.strptime(datestr, "%d.%m.%Y").strftime('%s')
+	print(date)
+	print(type(date))
+	date = int(date)
+	print(date)
+	bot.send_message(message.chat.id, "Скачиваю с даты: "+datestr)
+	st=pb.connectPrivat()
+	pb.editPrivat(st)
+	print("connected to privat")
+	pb.connectMonobank()
+
+	print(date)
+	transDB = sqlite3.connect("transactions2.db")
+	cursor=transDB.cursor()
+	cursor.execute("SELECT * FROM transactions WHERE transtime >"+str(date)+" AND operationAmount < 0 ORDER BY transtime ASC")
+	rows = cursor.fetchall()
+	bot.send_message(message.chat.id, "Расходы:")
+	moneysum=0
+	for row in rows:
+		print(row)
+		moneysum=moneysum+row[7]
+		bot.send_message(message.chat.id, f"""Транзакция с карты: {row[1]}
+		Дата: {datetime.datetime.utcfromtimestamp(row[3]).strftime("%d.%m.%Y")}
+		Описание: {row[4]}
+		Сумма: {str(row[7])} {row[8]}
+		Баланс: {row[11]}""")
+	bot.send_message(message.chat.id,"Расходы в сумме: "+str(moneysum))
+	cursor.execute("SELECT * FROM transactions WHERE transtime >"+str(date)+" AND operationAmount > 0 ORDER BY transtime ASC" )
+	rows = cursor.fetchall()
+	bot.send_message(message.chat.id, "Доходы:")
+	moneysum=0
+	for row in rows:
+		if row[8]==980 or row[8]=="UAH" or row[8]=="980":
+			currency="UAH"
+		else:
+			currency=row[8]
+		bot.send_message(message.chat.id, f"""Транзакция с карты: {row[1]}
+		Дата: {datetime.datetime.utcfromtimestamp(row[3]).strftime("%d.%m.%Y")}
+		Описание: {row[4]}
+		Сумма: {str(row[7])} {currency}
+		Баланс: {row[11]}""")
+		moneysum=moneysum+row[7]
+	bot.send_message(message.chat.id,"Доходы в сумме: +"+str(moneysum))
+	transDB.close()
+#график по дням недели
+
+@bot.message_handler(commands=['check'])
+
+#Проверка оплат от учеников
+def checkForPay(message):
+	transDB = sqlite3.connect("transactions2.db")
+	cursor=transDB.cursor()
+	cursor.execute("SELECT * FROM transactions WHERE cardNumber = 'monobankcard5555' ORDER BY transtime ASC")
+	Mrows=cursor.fetchall()
+	print(Mrows)
+	for row in Mrows:
+		date=str(datetime.datetime.utcfromtimestamp(row[3]).strftime("%d.%m.%Y"))
+
+		if "Від:" in row[4]:
+			print(row)
+			transFromWho=row[4].replace("Від: ", "")
+			print(transFromWho)
+			bot.send_message(message.chat.id, "Транзакция на карту МОНОБАНК в сумме "+str(row[6])+" UAH от "+transFromWho+" "+date)
+	cursor.execute("SELECT * FROM transactions WHERE cardNumber = '5363542012827151' ORDER BY transtime ASC")
+	#privat search
+	Prows=cursor.fetchall()
+	for row in Prows:
+		print(row)
+		date=str(datetime.datetime.utcfromtimestamp(row[3]).strftime("%d.%m.%Y"))
+		if "Перевод с карты ПриватБанка через приложение Приват24. Отправитель:"  in row[4] and "Перевод на свою «Копилку»" not in row[4]:
+			print(row)
+			bot.send_message(message.chat.id, f"Транзакция на карту {str(row[1])} Приват24  в сумме "+str(row[6])+". От  "+row[4].replace("Перевод с карты ПриватБанка через приложение Приват24. Отправитель:"," ")+" Дата: "+date)
+		elif "Зачисление перевода на карту" in row[4] and "Перевод на свою «Копилку»" not in row[4]:
+			print(row)
+			if row[7]==1000.0:
+				bot.send_message(message.chat.id, f"Транзакция на карту {str(row[1])}. Сумма: 1000 UAH от ЗНО Вадим. "+date)
+			elif "TRANSFERWISE" in row[4]:
+			 	bot.send_message(message.chat.id, f"Транзакция на карту {str(row[1])}. Сумма: {row[6]} от Анастасия "+date)
+			else:
+				bot.send_message(message.chat.id, f"Транзакция на карту {str(row[1])} Приват24  в сумме "+str(row[6])+". "+row[4].replace("Зачисление перевода на карту", " ")+date)
+				bot.send_message(message.chat.id, "Не удалось определить от кого. "+date)
+				#добавить реплай маркап с обозначением от кого транзакция
+
+
+
+
+
+
+
+
+
+
+	#задачи: создать ученика, создать количество уроков, создать длительность урока, сумму к оплате, статус оплаты
+
+
+
+
+
+
+
+bot.polling(none_stop=True, interval=0);
---
+++ onlyPrivatModule.py
@@ -0,0 +1,162 @@
+import requests
+import json
+import time
+import datetime
+import sqlite3
+import ast
+import hashlib
+#========= PrivatBank
+def connectPrivat():
+	urlForPrivat = "https://api.privatbank.ua/p24api/rest_fiz"
+	password = "dx4hL2c9K0U2116gMUlwUNVZ3hP8eLD6"
+	headers={'Content-Type': 'application/xml; charset=UTF-8'}
+	merchId="182739"
+	sd="10.10.2020"
+	ed=datetime.date.today().strftime("%d.%m.%Y")
+	head = """<?xml version="1.0" encoding="UTF-8"?><request version="1.0"><merchant><id>"""+merchId+"""</id><signature>"""
+
+	data = f"""<oper>cmt</oper>
+    	    <wait>0</wait>
+        	<test>0</test>
+        	<payment id="">
+        	<prop name="sd" value="{sd}" />
+        	<prop name="ed" value="{ed}" />
+        	<prop name="card" value="5363542012827151" />
+        	</payment>"""
+	end_head = """</signature></merchant><data>"""
+
+	footer = """</data></request>"""
+    # === Шифрование Ключа ====================================
+
+	signature_md5 = hashlib.md5((data+password).encode('utf-8')).hexdigest()
+	signature_done = hashlib.sha1(signature_md5.encode('utf-8')).hexdigest()
+
+	data_done = head + signature_done + end_head + data + footer
+# === Запрос ==============================================
+	askForTrans=requests.get(urlForPrivat,headers=headers, data=data_done)
+	print("Connecting to privat: "+str(askForTrans.status_code))
+	string=askForTrans.text
+	print(string)
+	#
+	#
+	#	Добавить обработку неверного ip
+	#
+	#
+	#
+#=======форматирование ответа от привата
+
+	return string
+
+def editPrivat(inputt):
+	#=======форматирование ответа от привата
+	string=inputt
+	#убирет хеадер 
+	i=0
+	while i<2:
+		i=i+1
+		string=string[string.find('">')+2:]
+	string=string[:len(string)-39].strip().split('/>')
+	del string[len(string)-1]
+	ress=string
+	#форматирование файла и приведение в удобный вид для создания списка с вложеным елементом словарь. в каждом словаре транзакция
+	i=-1
+	while i<(len(ress)-1) :
+		ress[i]=ress[i].replace("=",":")
+		ress[i]=ress[i].replace('" ','", ')
+		ress[i]=ress[i].replace('<statement card','{"statementCard"')
+		ress[i]=ress[i].replace('appcode','"appcode"')
+		ress[i]=ress[i].replace('trandate','"trandate"')
+		ress[i]=ress[i].replace('trantime','"trantime"')
+		ress[i]=ress[i].replace(' amount','"amount"')
+		ress[i]=ress[i].replace('cardamount','"cardamount"')
+		ress[i]=ress[i].replace('rest','"balance"')
+		ress[i]=ress[i].replace('terminal','"terminal"')
+		ress[i]=ress[i].replace('description','"description"')
+		ress[i]=ress[i]+"}"
+		ress[i]=eval(ress[i])
+		i=i+1
+	transDB = sqlite3.connect("transactions2.db")
+	cursor=transDB.cursor()
+	cursor.execute("""CREATE TABLE IF NOT EXISTS  transactions (ident INTEGER,cardNumber, transid PRIMARY KEY, transtime, description, category_mcc, amount,
+	 	operationAmount, currencyCode, comissionRate,
+	  cashbackAmount, balance, hold, category)""")
+	transDB.commit()
+
+	#privat
+	print(ress)
+	print(type(ress))
+	# for trans in ress:
+	# 	print(type(ress))
+	# 	print(ress[0])
+	# 	print(type(ress[0]))
+	for trans in ress:
+		#print(trans)
+		#print(type(trans))
+		
+		d=trans['trandate']
+		t=trans['trantime']
+		trans['cardamount']=trans['cardamount'].strip('UAH')
+		data=datetime.date(int(d[0]+d[1]+d[2]+d[3]),int(d[5]+d[6]),int(d[8]+d[9])).strftime('%s')
+		trans['trantime']=data
+		#print("transdate:")
+		#print(trans['trantime'])
+
+		cursor.execute('INSERT OR IGNORE INTO transactions (cardNumber, transid, transtime, description, category_mcc, amount,operationAmount, currencyCode, comissionRate, cashbackAmount, balance) VALUES (?,?,?,?,?,?,?,?,?,?,?)  ', [trans['statementCard'], trans['appcode'], int(trans['trantime']),trans['description'], "unknown", trans['amount'], float(trans['cardamount']), " UAH", "unknown comission", "noCashback", trans['balance']] )
+		transDB.commit()
+
+	cursor.execute("SELECT * FROM transactions ORDER BY transtime")
+	rows = cursor.fetchall()
+	for row in rows: print(row)
+	cursor.execute('SELECT * FROM transactions ORDER BY transtime ASC')
+	transDB.commit()
+
+
+
+
+#======monobank========
+def connectMonobank(xToken ="uOqu2q0nXFpyNpa3DBnYsYmT4o_GgXpL2-n7kowtbnVE"):
+	 #xToken Ключ доступа с личного кабинета монобанка
+	connectionStatusCodes ={100:'Informational', 200: 'Connection OK', 300:'Redirection', 400:"Client error", 500:'Server error'}
+	urlForAccMono = 'https://api.monobank.ua/personal/client-info'
+	headers = {'content-type': 'application/json', 'X-Token': xToken}
+	
+	request = requests.get(urlForAccMono, headers=headers)
+	statuscode=request.status_code
+
+	if statuscode in connectionStatusCodes:
+		print("Подключение к серверу monobank'а: "+ connectionStatusCodes.get(statuscode))
+	else: 
+		print("Подключение к серверу monobank'а: "+ str(statuscode))
+
+	monoAccountDict=request.json()
+	edate=datetime.datetime.now()#время сейчас
+	eunixt=time.time()
+	sunixt=int(eunixt-2629743.0)
+	monoAccountDict=request.json()
+	urlForTransMono = 'https://api.monobank.ua/personal/statement/'+monoAccountDict['accounts'][1]['id']+'/'+str(sunixt)
+	headers = {'content-type': 'application/json', 'X-Token': xToken}
+	req2=requests.get(urlForTransMono, headers=headers)
+
+	statuscode2=req2.status_code
+	if statuscode2 in connectionStatusCodes:
+		print("Подключение к серверу monobank'а: "+ connectionStatusCodes.get(statuscode2))
+	else: 
+		print("Подключение к серверу monobank'а: "+ str(statuscode2))
+
+	mtransList = req2.json()
+	# ВСТАВИТЬ ПРОВЕРКИ НА ПУСТОЙ СПИСОК
+	# if mtransList==[]:  
+	# 	print("пустой список")
+	transDB = sqlite3.connect("transactions2.db")
+	cursor=transDB.cursor()
+	cursor.execute("""CREATE TABLE IF NOT EXISTS  transactions (ident INTEGER,cardNumber, transid PRIMARY KEY, transtime, description, category_mcc, amount,
+	 	operationAmount, currencyCode, comissionRate,
+	  cashbackAmount, balance, hold, category)""")
+	transDB.commit()
+	for trans in mtransList:
+		print(trans)
+		cursor.execute('INSERT OR IGNORE INTO transactions (cardNumber, transid, transtime, description, category_mcc, amount,operationAmount, currencyCode, comissionRate, cashbackAmount, balance) VALUES (?,?,?,?,?,?,?,?,?,?,?) ', ["monobankcard5555",trans['id'], trans['time'], trans['description'], trans['mcc'], trans['amount']/100, trans['operationAmount']/100, trans['currencyCode'], trans['commissionRate']/100, trans['cashbackAmount']/100, trans['balance']/100] )
+	cursor.execute('SELECT * FROM transactions ORDER BY transtime ASC')
+	transDB.commit()
+
+
